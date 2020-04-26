a4026eedb5abe3122c2109f02fe980b9d839c4ae
idilsaglam <idilsaglam1@hotmail.com>
2020-04-26 12:00:18 +0300

    eslint issues fixed

--- src/selectors/facebook.js
+++ src/selectors/facebook.js
@@ -1,13 +1,13 @@
 module.exports = {
-    'login_form': {
-        'email': '#login_form input[type="email"]',
-        'password': '#login_form input[type="password"]',
-        'submit': '#login_form input[type="submit"]',
-        "parent": "#login_form"
-    },
-    'facebook_group': {
-        'm_group_stories_container': '#m_group_stories_container',
-        'm_group_post_div': '#m_group_stories_container .storyStream article',
-        'm_group_story_container': '#m_group_stories_container .storyStream article .story_body_container'
-    }
-}
+  'login_form': {
+    'email': '#login_form input[type="email"]',
+    'password': '#login_form input[type="password"]',
+    'submit': '#login_form input[type="submit"]',
+    'parent': '#login_form',
+  },
+  'facebook_group': {
+    'm_group_stories_container': '#m_group_stories_container',
+    'm_group_post_div': '#m_group_stories_container .storyStream article',
+    'm_group_story_container': '#m_group_stories_container .storyStream article .story_body_container',
+  },
+};
