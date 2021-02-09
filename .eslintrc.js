7bbaa65605e38d0af701816408152899989946cd
kaanyagci <kaan.yagci@clique.paris>
2021-02-09 19:11:12 +0100

    ESLint config file changed

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: [
+      '@typescript-eslint',
+    ],
+    parserOptions: {
+        project: './tsconfig.json',
+    },
+    extends: [
+        'airbnb-typescript',
+    ],
+};
