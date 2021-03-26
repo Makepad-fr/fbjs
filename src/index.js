5384cbac1d12b2eec56c70c168705816fe584b6a
kaanyagci <kaan.yagci@clique.paris>
2021-03-26 01:06:24 +0100

    try/catch syntax issue fixed

--- src/index.js
+++ src/index.js
@@ -329,17 +329,17 @@ async function facebookLogIn(arguments, page, setPageListeners) {
   // Goes to base facebook url
   await page.goto('https://facebook.com');
   try {
-  await page.waitForXPath('//button[@data\-cookiebanner="accept_button"]');
-  const acceptCookiesButton = (
-    await page.$x('//button[@data\-cookiebanner="accept_button"]')
-  )[0];
-  await page.evaluate((el) => {
-    el.focus();
-    el.click();
-  }, acceptCookiesButton);
- } catch {
+    await page.waitForXPath('//button[@data\-cookiebanner="accept_button"]');
+    const acceptCookiesButton = (
+      await page.$x('//button[@data\-cookiebanner="accept_button"]')
+    )[0];
+    await page.evaluate((el) => {
+      el.focus();
+      el.click();
+    }, acceptCookiesButton);
+  } catch (error) {
     console.info('Cookie banner did not appear');
- }
+  }
   /**
    * Waiting for login form JQuery selector to avoid
    * that forms elements to be not found
