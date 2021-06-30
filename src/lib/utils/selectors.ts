5135796313b090d2f09a7722d17f2586674256a2
iMrDJAi <42304709+iMrDJAi@users.noreply.github.com>
2021-06-30 21:41:43 +0100

    Added desktop layout selectors

--- src/lib/utils/selectors.ts
+++ src/lib/utils/selectors.ts
@@ -10,4 +10,16 @@ export default {
     m_group_post_div: '#m_group_stories_container .storyStream article',
     m_group_story_container: '#m_group_stories_container .storyStream article .story_body_container',
   },
+  facebook_group_new: {
+    group_feed_container: 'div[role=feed]',
+    group_post_div: 'div[role=article][aria-labelledby]',
+    group_post_header: 'div[role=article][aria-labelledby] > div > div > div > div > div > :not(div:empty) > div > :not(div:empty)',
+    group_post_author: 'strong span a',
+    group_post_author_avatar: 'image',
+    group_post_link: 'span span span a',
+    group_post_content: 'div[dir=auto][class]',
+    group_post_attachment: 'div[dir=auto][class] + div',
+    group_post_video: 'video',
+    group_post_img: 'img',
+  }
 };
