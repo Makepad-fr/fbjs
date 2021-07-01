4e6279cd64d5f35b0ff18b50a4258f8e94cac26b
iMrDJAi <42304709+iMrDJAi@users.noreply.github.com>
2021-07-01 15:48:24 +0100

    Added xpath selectors + some improvements

--- src/lib/utils/selectors.ts
+++ src/lib/utils/selectors.ts
@@ -11,16 +11,31 @@ export default {
     m_group_story_container: '#m_group_stories_container .storyStream article .story_body_container',
   },
   facebook_group_new: {
-    group_feed_container: 'div[role=feed]',
-    group_post_div: 'div[role=article][aria-labelledby]',
-    group_post_header: 'div[role=article][aria-labelledby] > div > div > div > div > div > :not(div:empty) > div > :not(div:empty)',
-    group_post_author: 'strong span a',
-    group_post_author_avatar: 'image',
-    group_post_link: 'span span span a',
-    group_post_content: 'div[dir=auto][class]',
-    group_post_content_expand_button: 'div[role=button]',
-    group_post_attachment: 'div[dir=auto][class] + div',
-    group_post_video: 'video',
-    group_post_img: 'img',
+    css: {
+      group_name: 'html > head > title',
+      group_feed_container: 'div[role=feed]',
+      group_post_div: 'div[role=article][aria-labelledby]',
+      group_post_author: 'h2 span a',
+      group_post_author_avatar: 'object image',
+      group_post_link: 'span[dir=auto] > span a',
+      group_post_content: 'div[dir=auto][class]',
+      group_post_content_expand_button: 'div[role=button]',
+      group_post_attachment: 'div[dir=auto][class] + div[class][id]',
+      group_post_video: 'video',
+      group_post_img: 'img',
+    },
+    xpath: {
+      group_name: '/html/head/title',
+      group_feed_container: '//div[@role="feed"]"',
+      group_post_div: '//div[@role="article"][@aria-labelledby]',
+      group_post_author: '//h2//span//a',
+      group_post_author_avatar: '//object//*[name()="image"]',
+      group_post_link: '//span[@dir="auto"]/span//a',
+      group_post_content: '//div[@dir="auto"][@class]',
+      group_post_content_expand_button: '//div[@role="button"]',
+      group_post_attachment:'//div[@role]//div[@role="article"][@aria-labelledby]//div[@dir="auto"][@class]//following::div[1][@class][@id]',
+      group_post_video: '//video',
+      group_post_img: '//img'
+    }
   }
 };
