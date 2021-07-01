4398ea47bfa62dcbcd32063ef3323bfb6fb5b006
iMrDJAi <42304709+iMrDJAi@users.noreply.github.com>
2021-07-01 18:59:27 +0100

    Small fix

--- src/lib/utils/selectors.ts
+++ src/lib/utils/selectors.ts
@@ -26,16 +26,16 @@ export default {
     },
     xpath: {
       group_name: '/html/head/title',
-      group_feed_container: '//div[@role="feed"]"',
+      group_feed_container: '//div[@role="feed"]',
       group_post_div: '//div[@role="article"][@aria-labelledby]',
       group_post_author: '//h2//span//a',
       group_post_author_avatar: '//object//*[name()="image"]',
       group_post_link: '//span[@dir="auto"]/span//a',
       group_post_content: '//div[@dir="auto"][@class]',
       group_post_content_expand_button: '//div[@role="button"]',
-      group_post_attachment:'//div[@role]//div[@role="article"][@aria-labelledby]//div[@dir="auto"][@class]//following::div[1][@class][@id]',
+      group_post_attachment: '//div[@role]//div[@role="article"][@aria-labelledby]//div[@dir="auto"][@class]//following::div[1][@class][@id]',
       group_post_video: '//video',
-      group_post_img: '//img'
-    }
-  }
+      group_post_img: '//img',
+    },
+  },
 };
