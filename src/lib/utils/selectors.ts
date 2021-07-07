d2ba264702696f5765ebc1ec0a6f1dd603d46503
Kaan Yagci <9104546+kaanyagci@users.noreply.github.com>
2021-07-07 16:52:27 +0200

    [4.1.0] - Migration to the Desktop site (#44)
    * Switching to the desktop layout (work in progress) (#41)
    
    * :memo: Funding documentation added (#40)
    
    * Added desktop layout selectors
    
    * Added "See More" button selector
    
    * Added xpath selectors + some improvements
    
    * Small fix
    
    * README updated (#42)
    
    npm badges added
    
    * Checkpoint
    
    This is completely a mess! The function `getGroupPosts()` needs a full rewrite!
    
    * Updated the scraper code
    
    - A full rewrite for the scraper function.
    - MutationObserver implementation.
    
    * Improvements!
    
    - Fixed a bug when posts don't have  text content.
    - Now it clicks on the "See more..." button before extracting the post content!
    
    * Removed xpath selectors + a bunch of minor changes
    
    Co-authored-by: Kaan Yagci <9104546+kaanyagci@users.noreply.github.com>
    
    * docs: add iMrDJAi as a contributor for code (#43)
    
    * :memo: Funding documentation added (#40)
    
    * README updated (#42)
    
    npm badges added
    
    * docs: update README.md [skip ci]
    
    * docs: update .all-contributorsrc [skip ci]
    
    * Update README.md for missing badges
    
    * Duplicated all contributors badge removed
    
    Co-authored-by: Kaan Yagci <9104546+kaanyagci@users.noreply.github.com>
    Co-authored-by: allcontributors[bot] <46447321+allcontributors[bot]@users.noreply.github.com>
    
    * feat(package): sponsor button added to the npm package
    
    * fix(browser): facebook headless browser rejection issue fixed
    
    user agents added
    
    * feat: concom configuration added
    
    Concom is a Conventional Commit formatter which is actually in private alpha release
    
    * feat(version): Version incremented to 4.1.0
    
    Co-authored-by: ${Mr.DJA} <42304709+iMrDJAi@users.noreply.github.com>
    Co-authored-by: allcontributors[bot] <46447321+allcontributors[bot]@users.noreply.github.com>

--- src/lib/utils/selectors.ts
+++ src/lib/utils/selectors.ts
@@ -6,8 +6,20 @@ export default {
     parent: 'form',
   },
   facebook_group: {
-    m_group_stories_container: '#m_group_stories_container',
-    m_group_post_div: '#m_group_stories_container .storyStream article',
-    m_group_story_container: '#m_group_stories_container .storyStream article .story_body_container',
+    group_name: 'title',
+    group_feed_container: 'div[role=feed]',
+  },
+  facebook_post: {
+    post_element: 'div[role=article][aria-labelledby]',
+    post_author: 'h2 span a',
+    post_author2: 'h2 strong span',
+    post_author_avatar: 'object image',
+    post_link: 'span[dir=auto] > span a',
+    post_content: 'div[dir=auto][class]',
+    post_content_expand_button: 'div[role=button]',
+    post_attachment: 'div[dir=auto][class] + div[class][id]',
+    post_attachment2: 'div[class][id]',
+    post_video: 'video',
+    post_img: 'img',
   },
 };
