10569f79805e499d5f1584a2b7c948b36a81ea04
kaanyagci <kaan.yagci@clique.paris>
2021-09-28 23:33:01 +0200

    :sparkles: Group module exported from library

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { default as Configuration } from './lib/models/configuration';
 export { default as Options } from './lib/models/options';
 export { default as InitialisationError } from './lib/errors/initialisationError';
 export { default as TwoFARequiredError } from './lib/errors/twoFARequiredError';
+export { default as Group } from './lib/models/group';
