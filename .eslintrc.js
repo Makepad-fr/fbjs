19551b336ee4648271d2c27e887ab767d9629808
kaanyagci <kaan.yagci@clique.paris>
2021-09-29 23:00:50 +0200

    :wrench: linter configuration updated
    import extensions rule updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,18 @@ module.exports = {
     parserOptions: {
         project: './tsconfig.json',
     },
+    rules: {
+        "import/extensions": [
+            "error",
+            "ignorePackages",
+            {
+                "js": "never",
+                "jsx": "never",
+                "ts": "never",
+                "tsx": "never"
+            }
+        ]
+    },
     settings: {
         react: {
             version: "999.999.999"
